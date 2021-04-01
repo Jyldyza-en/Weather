81f28e541aea5d56950fe59e5dfafa3a5b6a5ed8
Jyldyz Sultanbekova <sultanbekova.jyldyz@gmail.com>
2021-04-01 08:56:13 +0600

    practice with TextView and EditText

--- app/src/main/java/kg/tutorialapp/weather/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/weather/MainActivity.kt
@@ -2,10 +2,21 @@ package kg.tutorialapp.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var text1 = findViewById<TextView>(R.id.textView)
+        text1.text = getString(R.string.greeting)
+
+        var edit = findViewById<EditText>(R.id.editText)
+
+        text1.setOnClickListener {
+            text1.text = edit.text
+        }
     }
 }
