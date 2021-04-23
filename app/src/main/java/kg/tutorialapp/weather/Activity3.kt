0baa019812b3b33aadad496ba6b897591939e89b
Jyldyz Sultanbekova <sultanbekova.jyldyz@gmail.com>
2021-04-23 21:39:49 +0600

    familiarity with the life cycle of an activity

--- app/src/main/java/kg/tutorialapp/weather/Activity3.kt
+++ app/src/main/java/kg/tutorialapp/weather/Activity3.kt
@@ -2,16 +2,52 @@ package kg.tutorialapp.weather
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class Activity3: AppCompatActivity() {
+
+    val TAG = "LIFECYCLE"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity3)
         setupView()
+        Log.d(TAG, "Activity onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Activity onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Activity onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Activity onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Activity onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "Activity onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "Activity onDestroy")
+    }
+
+
 
     private fun setupView() {
         val butt = findViewById<Button>(R.id.button16)
